7de967d24fe6a66d0bf8c4fb7281884851bd98b1
SunRey Eagle <sunrey@localhost.localdomain>
2018-07-22 22:01:45 -0600

    Applied and added PHP Slim 3 libraries, and modified the home.php file to use subpages with new directories. At the moment, the only subpages we're using is for header and footer. Rendered site does not appear modified yet, but with the framework in place we can start to create with templates.

--- index.php
+++ index.php
@@ -1,88 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-GB">
-<head>
-	<title>Server Status</title>
-	<link rel="stylesheet" type="text/css" href="css/screen.css" media="screen" />
-	<meta name="description" content = "Nerd hub for nerds xoxoxo">
-	<meta name="keywords" content="wow,gamenight,nerds,minecraft,davis,vischa,nerdboi">
-	<meta name="author" content="Love, Chen hehehe">
-</head>
-<body>
+<?php
+require 'vendor/autoload.php';
 
-<div id="header">
-	<center>
-		<h2>menu maybe?</h2>
-	</center>
-</div>
-<div class="colmask rightmenu">
-	<div class="colleft">
-		<div class="col1">
-		
-			<!-- Column 2 start -->
-			<div id="ads">
-			</div>
-			<h1>
-			<?php 
-			//phpinfo();
-			include 'functions.php';
-			$serverName = "vischa.ddns.net";
-			$serverIP = gethostbyname($serverName);
-			$mangosPort = 3724; //Realm Handler
-			$realmPort_1 = 8085; //Realm World
-			$minecraftPort_1 = 25565; //Minecraft Server #1
+//use \Psr\Http\Message\ServerRequestInterface as Request;
+//use \Psr\Http\Message\ResponseInterface as Response;
 
-			if (stest($serverIP, $mangosPort)) { //Realm Handler
-				echo "<font color=#008800>The MaNGOS server is up!</font> </br>";
-			} else {
-				echo "<font color=#CC0000>The MaNGOS server is down!</font> </br>";
-			}
+//use Slim\Http\Request;
+//use Slim\Http\Response;
 
-			if (stest($serverIP, $realmPort_1)) { //Realm World
-				echo "<font color=#008800>The Elwynn Forest realm is up!</font> </br>";
-				$wowUp = true;
-			} else {
-				echo "<font color=#CC0000>The Elwynn Forest realm is down!</font> </br>";
-				$wowUp = false;
-			}
 
-			if (stest($serverIP, $minecraftPort_1)) { //Minecraft Server #1
-				echo "<font color=#008800>The MineCraft Server is up! </font> </br>";
-			} else {
-				echo "<font color=#CC0000>The MineCraft Server is down!</font> </br>";
-			}
-			
-			if (stest($serverIP, 65022)) { //SSH port
-				echo "<font color=#008800>SSH port is Open </font> </br>";
-			} else {
-				echo "<font color=#CC0000>SSH port is Closed</font> </br>";
-			}
-			?>
-			</h1>
-			<!-- Column 2 end -->
-		</div>
-		<div class="col2">
-			<!-- Column 1 start -->
-			<center>
-				<img src="img\wotlk-logo.png" width="85%">
-				<h1>Server Stats:</h1>
-			</center>
-			<?php
-			//echo "</br></br></br>";
-			if ($wowUp) {
-				include 'soap.php';				
-			}
-			//echo "</br></br> ServerUP is: $wowUp";
+// instantiate the App object
+$app = new \Slim\App;
+$container = $app->getContainer();
 
-			?>
-			<!-- Column 1 end -->
-		</div>
-	</div>
-</div>
-<div id="footer">
-	<center>
-		Last updated 2018-07-13
-	</center>
-</div>
+$container['view'] = function ($container) {
+    $templates = '/srv/gate-house/templates';
+    return new \Slim\Views\PhpRenderer('/srv/gate-house');
+//    return new Slim\Views\Twig($templates, compact('cache'));
+};
 
-</body>
-</html>
+//$loader = new Twig_Loader_Filesystem('/srv/gate-house/templates');
+//$twig = new Twig_Environment($loader, array( 'debug' => true ));
+
+// Add route callbacks
+$app->get('/', function ($request, $response, $args) {
+    //global $twig;
+    //return $twig->render('home.twig', array( 'test_message' => '<p>--Switchfoot <\\p>' ));
+    return $this->view->render($response, 'pages/home.php' , [ 'arguments' => 'Hello World' ] );
+  });
+
+// Run application
+$app->run();
+
+?>
