c3845e3bb05fe73e43291a279ba8f2695fb2bd0a
Firewallzer0 <drtrask@gmail.com>
2019-03-03 19:34:57 -0700

    Updated password change files.

--- src/chpasswd-function.php
+++ src/chpasswd-function.php
@@ -6,6 +6,13 @@
 
   if ($password1 == $password2){
     //Update Password in Database
+    $userid = strtoupper($user);
+    $password1 = strtoupper($password1);
+    $srp = $user, ":", $password1;
+    $passhash = sha1($srp)
+    $update = "UPDATE account SET sha_pass_hash='$passhash' WHERE username='$user' AND sha_pass_hash='$passhash';"
+    $conn = new mysqli();
+    mysql
     //Update V and S using SRP6 Challenge
 
   } else {
--- src/db_config.php
+++ src/db_config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'webro');
-   define('DB_PASSWORD', '9peq7M4ocsyAR9OVVSd9');
-   define('DB_DATABASE', 'realm');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $DB_SERVER = "localhost:3306";
+   $DB_USERNAME = "webro";
+   $DB_PASSWORD = "9peq7M4ocsyAR9OVVSd9";
+   $DB_DATABASE = "realm";
+   $conn = new mysqli($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 ?>
